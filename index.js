c2d376ca86afc28d630fe9692605d2061b499588
unknown <vijay.nsut@gmail.com>
2020-05-07 11:41:08 +0530

    js file added

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
-const input=document.querySelector("input");
-const log=document.querySelector("#id");
-input.addEventListener("invalid",logValue);
+const claimBox= document.querySelector(".claim-box");
+const inputs=document.querySelectorAll("input");
 
-function logValue(e){
-	log.textContent +=e.target.value;
-}
+
+
+
+
+
+
+let flg=false;
+const claimBoxHandler=()=>{
+    const input1 = inputs[0].value;
+    const input2 = inputs[1].value;
+    const email=document.getElementById('email2');
+    const para= document.querySelector(".warn");
+    const input3 = inputs[2].value;
+    const input4 = inputs[3].value;
+ if (input1.trim()==="" ||input2.trim()==="" ||input3.trim()==="" ||input4.trim()===""  )
+   {
+     flg=true;
+ 
+   }
+
+
+   if (flg===true){
+      para.classList.add("visibility");
+      para.classList.remove("warn");
+      email.setAttribute('placeholder', "example@abc.com");
+
+  }
+
+  
+    
+
+
+
+
+};
+
+
+
+
+
+
+claimBox.addEventListener("click", claimBoxHandler);
