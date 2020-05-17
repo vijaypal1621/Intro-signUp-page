f66e8f80143069148d7f2d130f10f07166b45c70
unknown <vijay.nsut@gmail.com>
2020-05-17 20:17:43 +0530

    form is responding

--- index.js
+++ index.js
@@ -1,45 +1,24 @@
 const claimBox= document.querySelector(".claim-box");
 const inputs=document.querySelectorAll("input");
 
-
-
-
-
-
-
-let flg=false;
-const claimBoxHandler=()=>{
+const claimBoxHandler=(event)=>{
     const input1 = inputs[0].value;
     const input2 = inputs[1].value;
     const email=document.getElementById('email2');
-    const para= document.querySelector(".warn");
+    const para= document.querySelectorAll(".warn");
     const input3 = inputs[2].value;
     const input4 = inputs[3].value;
  if (input1.trim()==="" ||input2.trim()==="" ||input3.trim()==="" ||input4.trim()===""  )
    {
-     flg=true;
+      para.forEach( i =>{   
+         i.classList.add("visibility");
+     });
+      event.preventDefault();
+      email.setAttribute('placeholder', "example@abc.com");
  
    }
 
-
-   if (flg===true){
-      para.classList.add("visibility");
-      para.classList.remove("warn");
-      email.setAttribute('placeholder', "example@abc.com");
-
-  }
-
-  
-    
-
-
-
-
 };
-
-
-
-
 
 
 claimBox.addEventListener("click", claimBoxHandler);
